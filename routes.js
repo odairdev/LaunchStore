437c9800137092262ea032cd3e01a72aafabe437
odairdev <odair.dev@gmail.com>
2020-07-20 22:34:24 -0300

    First Commit

---
+++ routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const routes = express.Router()
+const ProductController = require('./src/App/controllers/ProductController')
+
+routes.get('/', (req, res) => {res.render('home')})
+routes.get('/products/create', ProductController.create)
+routes.get('/ads/create', (req, res) => {res.redirect('/products/create')})
+
+module.exports = routes
